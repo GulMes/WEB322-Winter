4e36386653e6ba0c2ddf01f12c1f1ed839ac62aa
Gulhan <gmesrefoglu@myseneca.ca>
2020-04-18 22:42:55 -0400

    Connected MongoDB Database

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
 const express = require("express");
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 
-//load the environment variable file
+//load the environment variables from the keys.env
 require('dotenv').config({path:"./config/keys.env"});
 
+//import router objects
+const homeRoutes = require("./controllers/homeController");
+const loginRoutes = require("./controllers/loginController");
+const productRoutes = require("./controllers/productController");
+const registerRoutes = require("./controllers/registerController");
+
+//creation of app object
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//express static middleware
+app.use(express.static('public'));
 
 //Handlebars middleware 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+//map Express to all router objects
+app.use("/",homeRoutes);
+app.use("/login",loginRoutes);
+app.use("/product",productRoutes);
+app.use("/register",registerRoutes);
 
-app.use(express.static('public'));
 
-//load controllers
-const homeController = require("./controllers/homeController");
-const loginController = require("./controllers/loginController");
-const productController = require("./controllers/productController");
-const registerController = require("./controllers/registerController");
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log(`Connected to MongoDB Database`);
+})
+.catch(err=>console.log(`Error occured when connecting to database ${err}`));
 
-//map each controller to the app object
-app.use("/",homeController);
-app.use("/Login",loginController);
-app.use("/Product",productController);
-app.use("/Register",registerController);
 
 //Set up server
 const PORT = process.env.PORT;
